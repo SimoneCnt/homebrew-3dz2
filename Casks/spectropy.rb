55ba3065e54a58d28a05311c97229559848fa1ef
Simone Conti <simonecnt@gmail.com>
2025-04-05 11:05:43 -0700

    Fix sha256

--- Casks/spectropy.rb
+++ Casks/spectropy.rb
@@ -3,7 +3,7 @@ cask "spectropy" do
     desc "Spectropy is a simple tool to view, compare and match Raman spectra of minerals."
     homepage "https://github.com/SimoneCnt/spectropy"
     version "2.0.1"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "c88f28e671348f930b3ec3a2f8e0a35e295cc924544cdb60629f6a1cb7e8a526"
     url "https://github.com/SimoneCnt/spectropy/releases/download/v#{version}/Spectropy.dmg"
     depends_on formula: "simonecnt/3dz2/spectropy-cli"
     app "Spectropy.app"
